da453e9c88179cfb6281aa8a6adf3b27e09a3818
Choi kangHyeon <khchoi@konahealth.co.kr>
2022-02-05 13:19:56 +0900

    업데이트

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,10 +10,8 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class Main {
     public static Map<String, List<Profile.Section>> items = new HashMap<>();
@@ -141,6 +139,8 @@ public class Main {
 
         public MainFrame()
         {
+            //setLocationByPlatform(true);
+            //setLocationRelativeTo(null);
             setResizable(false);
             setUndecorated(true);
 
@@ -218,10 +218,50 @@ public class Main {
         JLabel j5;
         JLabel j6;
         JLabel j7;
+        JButton j4_;
+        JButton j5_;
+        JButton j6_;
 
         JTextField tf = new JTextField(100);
+        JTextField sf = new JTextField(200);
 
         Button barLenBtn;
+        Button searchBtn;
+
+        String searchText1;
+        String searchText2;
+        String searchText3;
+
+        MouseAdapter mouseAdapter1 = new MouseAdapter() {
+            public void mouseClicked(MouseEvent me) {
+                System.out.println("1");
+                System.out.println("me = " + me.getSource());
+                sf.setText(searchText1);
+                ActionEvent actionEvent = new ActionEvent(searchBtn, ActionEvent.ACTION_PERFORMED, "1");
+                ActionListener actionListener = searchBtn.getActionListeners()[0];
+                actionListener.actionPerformed(actionEvent);
+            }
+        };
+        MouseAdapter mouseAdapter2 = new MouseAdapter() {
+            public void mouseClicked(MouseEvent me) {
+                System.out.println("2");
+                System.out.println("me = " + me.getSource());
+                sf.setText(searchText2);
+                ActionEvent actionEvent = new ActionEvent(searchBtn, ActionEvent.ACTION_PERFORMED, "1");
+                ActionListener actionListener = searchBtn.getActionListeners()[0];
+                actionListener.actionPerformed(actionEvent);
+            }
+        };
+        MouseAdapter mouseAdapter3 = new MouseAdapter() {
+            public void mouseClicked(MouseEvent me) {
+                System.out.println("3");
+                System.out.println("me = " + me.getSource());
+                sf.setText(searchText3);
+                ActionEvent actionEvent = new ActionEvent(searchBtn, ActionEvent.ACTION_PERFORMED, "1");
+                ActionListener actionListener = searchBtn.getActionListeners()[0];
+                actionListener.actionPerformed(actionEvent);
+            }
+        };
         public SecondFrame()
         {
             super("하프섭 규우");
@@ -260,19 +300,27 @@ public class Main {
             m_ListModel = new DefaultListModel<>();
             m_List = new JList<String>(m_ListModel);
             m_ScrollPane = new JScrollPane(m_List);
+            List<String> list = new ArrayList<>();
 
             for(String strKey : items.keySet()){
                 Set<String> strings = items.get(strKey).get(0).keySet();
                 for (String string : strings) {
-                    m_ListModel.addElement(string);
+                    //m_ListModel.addElement(string);
+                    list.add(string);
                 }
             }
 
+            Collections.sort(list);
+            //m_ListModel.removeAllElements();
+            for (String s : list) {
+                m_ListModel.addElement(s);
+            }
+
             add(m_ScrollPane);
-            m_ScrollPane.setBounds(10, 60, 250, 400);
+            m_ScrollPane.setBounds(10, 80, 250, 380);
 
             tf.setText(String.valueOf(barSize));
-            add(tf).setBounds(280, 20, 80, 30);
+            add(tf).setBounds(280, 10, 80, 30);
             barLenBtn = new Button("바 길이 조정");
             barLenBtn.addActionListener(new ActionListener() {
                 @Override
@@ -281,22 +329,50 @@ public class Main {
                     mf.setSize(barSize,10);
                 }
             });
-            add(barLenBtn).setBounds(370, 20, 100, 30);
+            add(barLenBtn).setBounds(370, 10, 100, 30);
 
-           j1 = new JLabel("요리 이름 : ");
+            //검색 기능
+            add(sf).setBounds(10, 40, 180, 30);
+            searchBtn = new Button("요리 검색");
+            searchBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    m_ListModel.clear();
+                    Set<String> strings;
+                    for(String strKey : items.keySet()){
+                        strings = items.get(strKey).get(0).keySet();
+                        for (String string : strings) {
+                            if(string.contains(sf.getText())){
+                                m_ListModel.addElement(string);
+                            }
+                        }
+                    }
+
+                    if(m_ListModel.size() == 0) JOptionPane.showMessageDialog(null, "일치하는 요리가 없습니다.", "요리 검색", JOptionPane.ERROR_MESSAGE);
+                }
+            });
+            add(searchBtn).setBounds(190, 40, 80, 30);
+
+            j1 = new JLabel("요리 이름 : ");
             j2 = new JLabel("조리법 : ");
             j3 = new JLabel("재료 : ");
             j4 = new JLabel("재료1");
             j5 = new JLabel("재료2");
             j6 = new JLabel("재료3");
             j7 = new JLabel("효과 : ");
+            j4_ = new JButton("1");
+            j5_ = new JButton("2");
+            j6_ = new JButton("3");
             add(j1).setBounds(280, 60, 500, 200);
             add(j2).setBounds(280, 100, 500, 200);
             add(j3).setBounds(280, 140, 500, 200);
             add(j4).setBounds(350, 140, 500, 200);
-            add(j5).setBounds(350, 160, 500, 200);
-            add(j6).setBounds(350, 180, 500, 200);
-            add(j7).setBounds(280, 240, 500, 200);
+            add(j4_).setLocation(350,140);
+            add(j5).setBounds(350, 170, 500, 200);
+            add(j5_).setLocation(350,170);
+            add(j6).setBounds(350, 200, 500, 200);
+            add(j6_).setLocation(350,200);
+            add(j7).setBounds(280, 250, 500, 200);
 
             // 이벤트 핸들러 등록
             JListHandler handler = new JListHandler();
@@ -305,13 +381,18 @@ public class Main {
         }
         class JListHandler implements ListSelectionListener
         {
-            String[] temp;
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 Set<String> strings;
+                j4.removeMouseListener(mouseAdapter1);
+                j5.removeMouseListener(mouseAdapter2);
+                j6.removeMouseListener(mouseAdapter3);
+                j4.setForeground(Color.black);
+                j5.setForeground(Color.black);
+                j6.setForeground(Color.black);
                 for(String strKey : items.keySet()){
                     if(items.get(strKey).get(0).get(m_List.getSelectedValue()) != null){
-                        temp = String.valueOf(items.get(strKey).get(0).get(m_List.getSelectedValue())).split("[|]");
+                        String[] temp = String.valueOf(items.get(strKey).get(0).get(m_List.getSelectedValue())).split("[|]");
 
                         j1.setText("요리 이름 : " + m_List.getSelectedValue());
                         j2.setText("조리법 : " + strKey);
@@ -319,8 +400,35 @@ public class Main {
                         j4.setText(temp[0] + "(" + temp[3] + "%)");
                         j5.setText(temp[1] + "(" + temp[4] + "%)");
                         j6.setText(temp[2] + "(" + temp[5] + "%)");
+
+                        /*
+                        for(String key : items.keySet()){
+                            if(items.get(key).get(0).get(temp[0]) != null){
+                                searchEvent(j4, temp[0]);
+                                j4.setForeground(Color.blue);
+                                searchText1 = temp[0];
+                                j4.addMouseListener(mouseAdapter1);
+                                System.out.println("searchText1 = " + searchText1);
+                            }
+                            if(items.get(key).get(0).get(temp[1]) != null){
+                                j5.setForeground(Color.blue);
+                                searchText2 = temp[1];
+                                j5.addMouseListener(mouseAdapter2);
+                                System.out.println("searchText2 = " + searchText2);
+                            }
+                            if(items.get(key).get(0).get(temp[2]) != null){
+                                j6.setForeground(Color.blue);
+                                searchText3 = temp[2];
+                                j6.addMouseListener(mouseAdapter3);
+                                System.out.println("searchText3 = " + searchText3);
+                            }
+                        }*/
+
                         if(temp.length > 6 ){
                             j7.setText("효과 : " + temp[6]);
+                        }
+                        else{
+                            j7.setText("효과 : ");
                         }
 
                         //System.out.println(mf);
@@ -342,9 +450,13 @@ public class Main {
             }
         }
 
+        private void searchEvent(JLabel jLabel ,String text) {
+        }
+
         @Override
         public void itemStateChanged(ItemEvent e) {
             m_ListModel.clear();
+            List<String> list = new ArrayList<>();
 
             Set<String> strings;
             switch (ch.getSelectedItem()) {
@@ -352,106 +464,135 @@ public class Main {
                     for(String strKey : items.keySet()){
                         strings = items.get(strKey).get(0).keySet();
                         for (String string : strings) {
-                            m_ListModel.addElement(string);
+                            //m_ListModel.addElement(string);
+                            list.add(string);
                         }
                     }
                     break;
                 case "혼합 (연습랭크 이상)" :
                     strings = items.get("혼합 (연습랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "굽기 (F랭크 이상)" :
                     strings = items.get("굽기 (F랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "삶기 (E랭크 이상)" :
                     strings = items.get("삶기 (E랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "반죽 (D랭크 이상)" :
                     strings = items.get("반죽 (D랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "끓이기 (C랭크 이상)" :
                     strings = items.get("끓이기 (C랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "면만들기 (B랭크 이상)" :
                     strings = items.get("면만들기 (B랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "튀기기 (A랭크 이상)" :
                     strings = items.get("튀기기 (A랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "볶기 (9랭크 이상)" :
                     strings = items.get("볶기 (9랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "파스타 만들기 (8랭크 이상)" :
                     strings = items.get("파스타 만들기 (8랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "잼 만들기 (7랭크 이상)" :
                     strings = items.get("잼 만들기 (7랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "파이 만들기 (6랭크 이상)" :
                     strings = items.get("파이 만들기 (6랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "찌기 (5랭크 이상)" :
                     strings = items.get("찌기 (5랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "피자 만들기 (4랭크 이상)" :
                     strings = items.get("피자 만들기 (4랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "발효 (3랭크 이상)" :
                     strings = items.get("발효 (3랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "수비드 (2랭크 이상)" :
                     strings = items.get("수비드 (2랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
                 case "저미기 (1랭크 이상)" :
                     strings = items.get("저미기 (1랭크 이상)").get(0).keySet();
                     for (String string : strings) {
-                        m_ListModel.addElement(string);
+                        //m_ListModel.addElement(string);
+                        list.add(string);
                     }
                     break;
+            }
+
+            /*
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < m_ListModel.size(); i++) {
+                list.add(m_ListModel.get(i));
+            }*/
+
+            Collections.sort(list);
+            //m_ListModel.removeAllElements();
+            for (String s : list) {
+                m_ListModel.addElement(s);
             }
         }
     }
